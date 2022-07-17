f025eff3a7cc31b78f87a430810f0add73e7ce0e
ajitshirsat <ajit.vikramrao.shirsat@gmail.com>
2022-07-17 15:49:51 +0530

    [Sanjay]: test class created

---
+++ src/sample/test.java
@@ -0,0 +1,15 @@
+package sample;
+
+public class test
+{
+	public static void main(String[] args) {
+		
+		System.out.println("Hi");
+		System.out.println("Hi");
+		System.out.println("Hi");
+		
+		
+		
+	}
+
+}
